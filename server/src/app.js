f49881a21c5715d7e07c170867d15ab41bfa1101
gabriel.nedelcu <gabriel.nedelcu2000@gmail.com>
2023-01-21 00:27:42 +0200

    server: added cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const cors = require("cors");
 
 const api = require("./routes/api");
 const errorHandler = require("./middleware/error.middleware");
@@ -7,6 +8,11 @@ const errorHandler = require("./middleware/error.middleware");
 const app = express();
 
 app.use(helmet());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 
 app.use("/v1", api);
